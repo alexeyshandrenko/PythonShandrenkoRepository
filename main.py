ad751251b3c9911fd9dbd7a57ffec711a7c3b74f
AlexeyShandrenko <70972424+AlexeyShandrenko@users.noreply.github.com>
2021-04-18 11:52:23 +0300

    start

---
+++ main.py
@@ -0,0 +1,20 @@
+# age = int(input("enter age "))
+#
+# if age >= 20:
+#     print("age > 20 and age =", age)
+# else:
+#     print("error")
+#     print("end!")
+#
+#
+# choice = input("enter choice ")
+#
+# while choice.lower() == "z":
+#     print("zzz")
+#     choice = input("enter z to continue or any to finish ")
+# print("end")
+
+
+
+
+
